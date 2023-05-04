c57a6afac56cfd4098d548ddf27830c676a9fd9b
RenanSC31 <renan.clemente19@gmail.com>
2023-05-04 11:55:00 -0300

    Aula de introdução 00

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,19 @@
-public class Main {
+public class Main{
     public static void main(String[] args) {
-        //laço contador-while
-        int cont=0;
-        while(cont<=100){
-            System.out.println("contador="+ cont);
-            //incremento
-            cont = cont+1;
-        }
-        //laço do para faça - for
-        for(int x=0;x<100;x++){
-            System.out.println("contador="+ x);
-            //incremento
-        }
-//laço fazer equanto-do while
-        int conta=1;
-        do{
-            cont++;
-            //processo
-        }while(conta<=33);
+        //Aula intradutória de Orientação à Objetos
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
