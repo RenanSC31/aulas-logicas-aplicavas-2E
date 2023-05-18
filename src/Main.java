525e917413e4cf5569473fa58fac9a66fcdec596
RenanSC31 <renan.clemente19@gmail.com>
2023-05-18 12:26:21 -0300

    Puxar uma classe

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,16 @@
+import java.util.Scanner;
+
 public class Main{
+
     public static void main(String[] args) {
-        //Aula intradutória de Orientação à Objetos
+        //Aula intradutória de Orientação à Objetos//
+        Pessoa adao = new Pessoa();//Declarção de objeto
+        Pessoa qualquer;
+        //instancição de objeto
+        qualquer = new Pessoa();
+        // Definição do formato da pessoa qualquerr
+        qualquer.nome = "Rita";
+        qualquer.sobrenome = "Lee";
+        qualquer.falar();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    }
-}
+    }}
---
+++ src/Pessoa.java
@@ -0,0 +1,10 @@
+public class Pessoa {
+ //Atributo da pessoa
+ public String nome;
+   public String sobrenome;
+    public void falar(){
+        System.out.println("falei");
+    }
+
+
+}
